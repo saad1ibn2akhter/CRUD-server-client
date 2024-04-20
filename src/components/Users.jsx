7f1b9168e676a7286f01a8b5863cf31c1480f966
Your Name <you@example.com>
2024-04-20 09:42:22 +0100

    delete form added

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -4,12 +4,26 @@ import { useLoaderData } from 'react-router-dom';
 const Users = () => {
     const users = useLoaderData();
     console.log(users);
+    const handleDelete = (id) =>{
+        console.log(id);
+
+        fetch(`http://localhost:5000/users/${id}`,{
+            method:'DELETE'
+        })
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+            if(data.deletedCount > 0){
+                alert("user deleted form the database ")
+            }
+        })
+    }
     return (
         <div>
             <h1>All users UI is here</h1>
             <h1>Total users : {users.length}</h1>
             {
-                users.map(user => <p key={user.id}>{user.name} : {user.email}</p>) 
+                users.map(user => <p key={user.id}>{user.name} : {user.email} <button onClick={() => handleDelete(user._id)}>X</button></p>) 
             }
         </div>
     );
